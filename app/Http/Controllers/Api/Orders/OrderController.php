121c421aaddc14c34331d9688dde72ce1d727f5d
MarcoAgosta <agostamarco@libero.it>
2023-03-09 16:32:52 +0100

    Creato token da chiamata api

--- app/Http/Controllers/Api/Orders/OrderController.php
+++ app/Http/Controllers/Api/Orders/OrderController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers\Api\Orders;
 
 use App\Http\Controllers\Controller;
+use Braintree\Gateway;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function generate(Request $request){
-        return 'generate';
+    public function generate(Request $request, Gateway $gateway){
+
+        $token= $gateway->clientToken()->generate();
+
+        $data=[
+            'token'=>$token
+        ];
+
+        return response()->json($data, 200);
     }
 
-    public function makePayment(Request $request){
+    public function makePayment(Request $request, Gateway $gateway){
         return 'makePayment';
     }
 }
